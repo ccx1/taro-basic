e8452e83badcd5d63e97178073d010849a2c0b25
chicunxiang <cunxiangchi@gmail.com>
2021-01-10 14:51:26 +0800

    修改路由

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,53 +1,68 @@
-const routes = [{
-  path: 'pages/home/index',
-  title: "首页",
-  addBar: true
+interface IRoute {
+    path: string;
+    title: string;
+    normalIcon?: string;
+    focusedIcon?: string;
+    addBar?: boolean;
+}
+
+const routes: Array<IRoute> = [{
+    path: 'pages/home/index',
+    normalIcon: './assets/home-unselected.png',
+    focusedIcon: './assets/home-selected.png',
+    title: "首页",
+    addBar: true
 }, {
-  path: 'pages/login/index',
-  title: "用户相关",
-  addBar: true
+    path: 'pages/login/index',
+    title: "用户相关",
+    normalIcon: './assets/user-unselected.png',
+    focusedIcon: './assets/user-selected.png',
+    addBar: true
 }, {
-  path: 'pages/index/index',
-  title: "测试"
+    path: 'pages/index/index',
+    title: "测试"
 }];
 
 
-function wrapRoutePath(routers: Array<any>) {
-  return routers.map((router) => (
-    router.path
-  ));
+function wrapRoutePath(routers: Array<IRoute>) {
+    return routers.map((router) => (
+        router.path
+    ));
 }
 
-function wrapNavigationRoutePath(routers: Array<any>) {
-  const filter = routers.filter((item: any) => !!item.addBar);
-  if (filter.length > 5) {
-    throw new Error('bottom bar item size() > 5')
-  }
-  return filter.map((router) => ({
-    pagePath: router.path,
-    text: router.title
-  }));
+function wrapNavigationRoutePath(routers: Array<IRoute>) {
+    const filter = routers.filter((item: any) => !!item.addBar);
+    if (filter.length > 5) {
+        throw new Error('bottom bar item size() > 5')
+    }
+    return filter.map((router) => ({
+        pagePath: router.path,
+        text: router.title,
+        iconPath: router.normalIcon,
+        selectedIconPath: router.focusedIcon
+    }));
 }
-
 
 export const pages = wrapRoutePath(routes);
 export const bottomNavigationBar = wrapNavigationRoutePath(routes);
 
 export default {
-  pages,
-  window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  },
-  networkTimeout: {
-    request: 10000,
-    downloadFile: 10000
-  },
-  tabBar: {
-    list: bottomNavigationBar
-  }
+    pages,
+    window: {
+        backgroundTextStyle: 'light',
+        navigationBarBackgroundColor: '#fff',
+        navigationBarTitleText: 'WeChat',
+        navigationBarTextStyle: 'black'
+    },
+    networkTimeout: {
+        request: 10000,
+        downloadFile: 10000
+    },
+    tabBar: {
+        selectedColor: '#d81e06',
+        color: '#8a8a8a',
+        list: bottomNavigationBar
+    }
 }
 
 
